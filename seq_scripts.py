f143117ab5d389846d909558e08f98442f734219
Rio-Nyx <58178550+Rio-Nyx@users.noreply.github.com>
2023-02-19 17:57:26 +0530

    updated changes

---
+++ seq_scripts.py
@@ -0,0 +1,73 @@
+import os
+import pdb
+import sys
+import copy
+import torch
+import numpy as np
+import torch.nn as nn
+from tqdm import tqdm
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+def seq_train(loader, model, optimizer, device, epoch_idx, recoder):
+    model.train()
+    loss_value = []
+    clr = [group['lr'] for group in optimizer.optimizer.param_groups]
+    for batch_idx, data in enumerate(loader):
+        vid = device.data_to_device(data[0])
+        vid_lgt = device.data_to_device(data[1])
+        label = device.data_to_device(data[2])
+        label_lgt = device.data_to_device(data[3])
+        ret_dict = model(vid, vid_lgt, label=label, label_lgt=label_lgt)
+        loss = model.criterion_calculation(ret_dict, label, label_lgt)
+        if np.isinf(loss.item()) or np.isnan(loss.item()):
+            print(data[-1])
+            continue
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        loss_value.append(loss.item())
+        if batch_idx % recoder.log_interval == 0:
+    optimizer.scheduler.step()
+    return loss_value
+
+def seq_eval(cfg, loader, model, device, mode, epoch, work_dir, recoder,
+             evaluate_tool="python"):
+    model.eval()
+    total_sent = []
+    total_info = []
+    total_conv_sent = []
+    stat = {i: [0, 0] for i in range(len(loader.dataset.dict))}
+    for batch_idx, data in enumerate(tqdm(loader)):
+        recoder.record_timer("device")
+        vid = device.data_to_device(data[0])
+        vid_lgt = device.data_to_device(data[1])
+        label = device.data_to_device(data[2])
+        label_lgt = device.data_to_device(data[3])
+        with torch.no_grad():
+            ret_dict = model(vid, vid_lgt, label=label, label_lgt=label_lgt)
+
+        total_info += [file_name.split("|")[0] for file_name in data[-1]]
+        total_sent += ret_dict['recognized_sents']
+        total_conv_sent += ret_dict['conv_sents']
+    # COMMENT START
+    try:
+        write2file(work_dir + "output-hypothesis-{}.ctm".format(mode), total_info, total_sent)
+        write2file(work_dir + "output-hypothesis-{}-conv.ctm".format(mode), total_info,
+                   total_conv_sent)
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        lstm_ret = 100.0
+    finally:
+        pass
+    return lstm_ret
+
+def write2file(path, info, output):
+    filereader = open(path, "w")
+    for sample_idx, sample in enumerate(output):
+        for word_idx, word in enumerate(sample):
+            filereader.writelines(
+                "{} 1 {:.2f} {:.2f} {}\n".format(info[sample_idx],
+                                                 word_idx * 1.0 / 100,
+                                                 (word_idx + 1) * 1.0 / 100,
+                                                 word[0]))
