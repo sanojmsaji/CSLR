6c149dbdf6c07a34cfc27e06aef3a78fd79a6f71
Rio-Nyx <58178550+Rio-Nyx@users.noreply.github.com>
2023-02-22 01:17:02 +0530

    minor fixes

--- modules/BiLSTM.py
+++ modules/BiLSTM.py
@@ -6,13 +6,13 @@ import torch.nn.functional as F
 class BiLSTMLayer(nn.Module):
     def __init__(self, input_size, debug=False, hidden_size=512, num_layers=1, dropout=0.3,
                  bidirectional=True, rnn_type='LSTM', num_classes=-1):
-        super(BiLtxtLayer, self).__init__()
+        super(BiLSTMLayer, self).__init__()
 
         self.dropout = dropout
         self.num_layers = num_layers
         self.input_size = input_size
         self.bidirectional = bidirectional
-        self.num_directions = 2 if bidirectional else 1
+        self.num_directions = 2
         self.hidden_size = int(hidden_size / self.num_directions)
         self.rnn_type = rnn_type
         self.debug = debug
@@ -24,7 +24,10 @@ class BiLSTMLayer(nn.Module):
             bidirectional=self.bidirectional)
 
     def forward(self, src_feats, src_lens, hidden=None):
-
+    '''
+        reduces the time for computation by a large extent
+        here each sequence has a different length
+    '''
         packed_emb = nn.utils.rnn.pack_padded_sequence(src_feats, src_lens)
 
         if hidden is not None and self.rnn_type == 'LSTM':
@@ -44,15 +47,3 @@ class BiLSTMLayer(nn.Module):
             "predictions": rnn_outputs,
             "hidden": hidden
         }
-
-    def _cat_directions(self, hidden):
-
-        def _cat(h):
-            return torch.cat([h[0:h.size(0):2], h[1:h.size(0):2]], 2)
-
-        if isinstance(hidden, tuple):
-            hidden = tuple([_cat(h) for h in hidden])
-        else:
-            hidden = _cat(hidden)
-
-        return hidden
